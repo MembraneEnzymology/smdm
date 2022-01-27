65cf8265723817544064c3a0ba72a27123e8be3f
MembraneEnzymology <77617528+MembraneEnzymology@users.noreply.github.com>
2022-01-27 21:31:01 +0100

    Add files via upload

---
+++ Simulations/run_sim_map_fit.py
@@ -0,0 +1,5 @@
+# check scripts.interactive_simulation to see instructions on where to store files and what kind of map to create
+
+import scripts.interactive_simulation as run
+
+last_diffusion_map = run.routine()
